a1924e564edeaae285c77dd40ce6216eefe34109
anselamkeykamona <sit96leminhduc@gmail.com>
2026-03-02 15:47:43 +0700

    Add room entity

---
+++ src/main/java/com/restaurant/qrorder/domain/entity/Rooms.java
@@ -0,0 +1,15 @@
+package com.restaurant.qrorder.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "rooms")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rooms {
+
+}
